55fbc0bb8731a6600243f07059b4db98f28f91ab
Meylie <emilie.desgranges78@gmail.com>
2026-03-13 13:53:50 +0100

    Update Player - handle rotation

--- project/Assets/Script/PlayerController.cs
+++ project/Assets/Script/PlayerController.cs
@@ -18,10 +18,20 @@ public class PlayerController : MonoBehaviour
 
     private void onMove()
     {
-        // TODO : rotate player to face movement direction
         Vector2 moveInput = m_moveAction.ReadValue<Vector2>();
+
+        if (moveInput == Vector2.zero) return;
+
+        // Player's rotation to face the direction of movement
+        Vector3 fromRotation = transform.rotation.eulerAngles;
+        float moveAngle = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg;
+        Vector3 toRotation = new Vector3(0f, moveAngle, 0f);
+        transform.rotation = Quaternion.Slerp( Quaternion.Euler(fromRotation), Quaternion.Euler(toRotation), 0.1f );
+
+        // Player's movement
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * m_moveSpeed * Time.deltaTime;
         transform.position += move;
+
         // TODO : add animation for movement
     }
 }
